700003b8360d67f2b932fd40ec29bdcf00ae4451
Maloney18 <lekansolomon18@gmail.com>
2025-03-26 11:28:38 +0100

    created all default exports

---
+++ src/app/(trendy)/movies/page.tsx
@@ -0,0 +1,8 @@
+
+const MoviesPage = () => {
+  return (
+    <div>coming soon</div>
+  )
+}
+
+export default MoviesPage
---
+++ src/app/(trendy)/upcoming/page.tsx
@@ -0,0 +1,8 @@
+
+const Upcoming = () => {
+  return (
+    <div>coming soon</div>
+  )
+}
+
+export default Upcoming
